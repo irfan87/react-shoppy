886eead3753272e51a514d367d7d10bd88a6aa50
Ahmad Irfan Mohammad Shukri <nerve2009@yahoo.com>
2017-12-31 16:52:31 +0800

    adding Bootstrap

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
 const About = () => (
-    <h3>About Us</h3>
+    <div>
+        <h3>About Us</h3>
+        <div>
+            <p>
+                Lorem ipsum dolor amet williamsburg snackwave pok pok gastropub 8-bit. Flannel brunch YOLO, tattooed craft beer raclette meggings subway tile. Succulents portland wolf ramps, butcher hell of vaporware air plant next level four dollar toast slow-carb neutra artisan dreamcatcher. Locavore bespoke tote bag neutra. Ennui farm-to-table crucifix, selfies direct trade paleo taxidermy tattooed tilde small batch.
+            </p>
+            <p>
+                Kickstarter green juice everyday carry edison bulb 8-bit four dollar toast la croix single-origin coffee bitters whatever vexillologist. Kinfolk small batch prism poke, chambray yr DIY tattooed knausgaard truffaut kale chips tofu. Poke banjo taiyaki, 8-bit air plant retro af pitchfork etsy heirloom mlkshk twee blog pop-up. Slow-carb man bun deep v brunch semiotics.
+            </p>
+            <p>
+                8-bit mustache live-edge franzen. Lyft celiac kale chips flannel twee direct trade four loko cred cold-pressed 90's waistcoat. Fashion axe cray shoreditch lumbersexual selfies pok pok forage sartorial vice authentic 90's intelligentsia celiac subway tile. Freegan chambray occupy deep v skateboard synth adaptogen tumblr vegan kinfolk offal gluten-free. Seitan tbh semiotics 8-bit pork belly coloring book selfies umami hammock.
+            </p>
+            <p>
+                IPhone hexagon scenester cardigan semiotics. Cold-pressed poke echo park, migas shabby chic yuccie austin tumeric umami tote bag fashion axe taxidermy hexagon put a bird on it. Venmo church-key lo-fi tattooed, street art microdosing humblebrag blog. Bitters neutra authentic normcore leggings. Copper mug iPhone polaroid, kinfolk literally YOLO 8-bit. Coloring book pabst mumblecore, brunch salvia synth meh yr pickled kinfolk sustainable palo santo.
+            </p>
+        </div>
+    </div>
 );
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,16 @@
 import React from 'react';
 
 const Home = () => (
-    <h3>Home Page</h3>
+    <div className="row">
+        <div className="container">
+            <div className="jumbotron">
+                <h2>Welcome to the Shoppy React</h2>
+                <p>
+                    We provide many products for all your daily usage.
+                </p>
+            </div>
+        </div>
+    </div>
 );
 
 export default Home;
